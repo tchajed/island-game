3fcc1f5e23ca30ab856dee96f7a5968219e45319
Mihir Kedia <meitros@gmail.com>
2015-01-18 16:10:02 -0500

    add support for win/lose, spacing fixes

--- static/game.js
+++ static/game.js
@@ -3,19 +3,26 @@ function log(text) {
 }
 
 $(function() {
-    var ws = new WebSocket("ws://localhost:8888/ws");
-    ws.onmessage = function(msg) {
-        console.log("client: " + msg);
-    };
-    ws.onopen = function() {
-        ws.send("hello server");
-    };
+  var ws = new WebSocket("ws://localhost:8888/ws");
+  ws.onmessage = function(msg) {
+    if (msg.indexOf("result") == 0) {
+      var won = msg.indexOf("won") != -1;
+      $( '#intro-message' ).addClass("hidden");
+      $( '#won_message' ).toggleClass("hidden", !won);
+      $( '#lost_message' ).toggleClass("hidden", won);
+    }
+    console.log("client: " + msg);
+  };
 
-    $.each( $( ".choice" ), function(_, raw_elem) {
-      var elem = $(raw_elem);
-      elem.click( function() {
-        $.post('http://localhost:8888/rpc/turn/' +
-                elem.data('item'));
-      });
+  ws.onopen = function() {
+    ws.send("hello server");
+  };
+
+  $.each( $( ".choice" ), function(_, raw_elem) {
+    var elem = $(raw_elem);
+    elem.click( function() {
+      $.post('http://localhost:8888/rpc/turn/' +
+              elem.data('item'));
     });
+  });
 });
