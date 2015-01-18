9049cab442a4e2f987f13e7093acc4656bba4594
Mihir Kedia <meitros@gmail.com>
2015-01-18 16:45:38 -0500

    client side support for ties, don't let person choose 2x per game

--- static/game.js
+++ static/game.js
@@ -1,15 +1,16 @@
-function log(text) {
-  console.log(text);
-}
-
 $(function() {
   var ws = new WebSocket("ws://localhost:8888/ws");
+  var parse_msg = function(s) { return s.split(":"); }
+
   ws.onmessage = function(msg) {
     if (msg.data.indexOf("result") == 0) {
-      var won = msg.data.indexOf("won") != -1;
+      var result = msg.data.split(":")[1];
       $( '#intro-message' ).addClass("hidden");
-      $( '#won_message' ).toggleClass("hidden", !won);
-      $( '#lost_message' ).toggleClass("hidden", won);
+      $( '#won_message' ).toggleClass("hidden", result != "won");
+      $( '#lost_message' ).toggleClass("hidden", result != "lost");
+      $( '#tie_message' ).toggleClass("hidden", result != "tie");
+      $( '#waiting_message' ).addClass("hidden");
+      $( '#choices ' ).removeClass("hidden");
     }
     if (msg.data.indexOf("uid:") === 0) {
         var uid = msg.data.slice(4);
@@ -27,6 +28,9 @@ $(function() {
     elem.click( function() {
       $.post('http://localhost:8888/rpc/turn/' +
               elem.data('item'));
+
+      $( '#waiting_message' ).removeClass("hidden");
+      $( '#choices ' ).addClass("hidden");
     });
   });
 });
