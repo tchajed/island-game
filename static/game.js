3e259277ca740aef6fde146623a8013c84123128
Tej Chajed <tchajed@gmail.com>
2015-01-18 15:21:31 -0500

    Echo over web sockets

--- static/game.js
+++ static/game.js
@@ -1,3 +1,13 @@
 function log(text) {
   console.log(text);
 }
+
+$(function() {
+    var ws = new WebSocket("ws://localhost:8888/ws");
+    ws.onmessage = function(msg) {
+        console.log("client: " + msg);
+    };
+    ws.onopen = function() {
+        ws.send("hello server");
+    };
+});
--- test.py
+++ test.py
@@ -1,14 +1,30 @@
+from __future__ import print_function
+
 import tornado.ioloop
 import tornado.web
+from tornado import websocket
 import os
 
 class MainHandler(tornado.web.RequestHandler):
   def get(self):
     self.render("index.html")
 
+class EchoWebSocketHandler(websocket.WebSocketHandler):
+    def open(self):
+        print("WebSocket opened")
+
+    def on_message(self, message):
+        print(message)
+        self.write_message("server: " + message);
+
+    def on_close(self):
+        print("WebSocket closed")
+
+
 application = tornado.web.Application(
 	[
     (r"/", MainHandler),
+    (r"/ws", EchoWebSocketHandler),
   ],
   template_path=os.path.join(os.path.dirname(__file__), "frontend"),
   static_path=os.path.join(os.path.dirname(__file__), "static"),
